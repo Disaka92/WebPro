2f04fffa72e843d757497413f0414bb8f4954bac
Tirosh Menuka <87849627+Disaka92@users.noreply.github.com>
2022-02-22 23:47:46 +0530

    Add php

--- Contact us page/form.php
+++ Contact us page/form.php
@@ -1,3 +1,36 @@
+<?php
+
+include 'config.php';
+
+  if (isset($_POST["submit"])) {
+    $username = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
+
+    $to = $email;
+    $subject = $message;
+
+    $message = "Name: {$username} Email: {$email} Phone: {$phone}  Message: " . $message;
+
+    // Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+    // More headers
+    $headers .= 'From: ___________@gmail.com';
+
+    $mail = mail($to,$subject,$message,$headers);
+
+    if ($mail) {
+      echo "<script>alert('Mail Send.');</script>";
+    }else {
+      echo "<script>alert('Mail Not Send.');</script>";
+    }
+  }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -48,7 +81,7 @@
               <span>Message</span>
             </div>
             
-            <input type="submit" value="Send" class="btn">
+            <input type="submit" name="submit" value="Send" class="btn" />
 
             
           </form>
